9694d8b7c1f30481da1b76cc16d2d79b84879bef
Aiden-Jeon <ells2124@gmail.com>
2021-03-30 11:25:12 +0900

    update docstyle

--- src/acmicpc/2021/9093.py
+++ src/acmicpc/2021/9093.py
@@ -1,12 +1,12 @@
 """
 title: 단어 뒤집기  
-problem: 9093  
-link: https://www.acmicpc.net/problem/9093  
+- problem: 9093  
+- link: https://www.acmicpc.net/problem/9093  
 
-Input: sentence with words
+- Input: sentence with words
     - 단어의 기준은 space
     - 대문자는 유지
-Output: 문장에서 단어들의 위치는 보존하고 각 단어들의 역순으로 이루어진 문장
+- Output: 문장에서 단어들의 위치는 보존하고 각 단어들의 역순으로 이루어진 문장
 """
 import sys
 
